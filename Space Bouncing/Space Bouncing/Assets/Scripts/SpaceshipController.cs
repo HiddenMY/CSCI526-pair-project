43c3e87577d01da6676fb0fbdee59879821d9ad2
JTJNF <ruoqiz@usc.edu>
2023-09-17 13:22:54 -0700

    Add end game message and launching angle restriction

--- Space Bouncing/Space Bouncing/Assets/Scripts/SpaceshipController.cs
+++ Space Bouncing/Space Bouncing/Assets/Scripts/SpaceshipController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpaceshipController : MonoBehaviour
 {
@@ -11,13 +12,18 @@ public class SpaceshipController : MonoBehaviour
     private int bounceTime;
     public int maxLandingTime = 3;
     public GameObject arrow; // Reference to your arrow GameObject
+    private Vector2? collisionNormal = null;
+    public TextMeshProUGUI endGameText;
 
     // Start is called before the first frame update
     void Start()
     {
+        endGameText.gameObject.SetActive(false);  // Initially hide the text
+    
         launched = false;
         bounceTime = 0;
         arrow.SetActive(true); // Show the arrow when the spaceship is to be launched
+
     }
 
     // Update is called once per frame
@@ -33,11 +39,16 @@ public class SpaceshipController : MonoBehaviour
         else
         {
             horizontalInput = Input.GetAxis("Horizontal");
-            transform.Rotate(Vector3.back, turnSpeed * horizontalInput * Time.deltaTime);
+
+            float desiredAngle = transform.rotation.eulerAngles.z - turnSpeed * horizontalInput * Time.deltaTime;
+            if (IsValidLaunchAngle(desiredAngle))
+            {
+                transform.Rotate(Vector3.back, turnSpeed * horizontalInput * Time.deltaTime);
+            }
         }
 
         // launching the spaceship
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !launched)
         {
             launched = true;
             arrow.SetActive(false); // Hide the arrow when the spaceship is launched
@@ -46,21 +57,22 @@ public class SpaceshipController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Border")
-        { // collision with the border
-            launched = false;
-            Debug.Log("You lose!");
-        }
 
+        if (collision.gameObject.tag == "Border")
+        {
+            launched = false;
+            endGameText.text = "You lose!";
+            endGameText.gameObject.SetActive(true);
+        }
+        else if (collision.gameObject.tag == "Finish")
+        {
+            launched = false;
+            endGameText.text = "You win!";
+            endGameText.gameObject.SetActive(true);
+        }
         else if (collision.gameObject.tag == "Planet")
         { // collision with planets
             CollideWithPlanet(collision);
-        }
-
-        else if (collision.gameObject.tag == "Finish")
-        { // collision with destination
-            launched = false;
-            Debug.Log("You win!");
         }
     }
 
@@ -73,6 +85,8 @@ public class SpaceshipController : MonoBehaviour
         angleZ = Mathf.Atan2(outDir.y, outDir.x);
         transform.rotation = Quaternion.Euler(0, 0, angleZ * Mathf.Rad2Deg - 90);
 
+        collisionNormal = collision.GetContact(0).normal;
+
         // deal with landing
         bounceTime++;
         if (bounceTime == maxLandingTime)
@@ -81,5 +95,22 @@ public class SpaceshipController : MonoBehaviour
             bounceTime = 0;
             arrow.SetActive(true); // Show the arrow when the spaceship is to be launched
         }
+    }
+
+    private bool IsValidLaunchAngle(float desiredAngle)
+    {
+        if (!collisionNormal.HasValue)
+        {
+            // If there's no collision normal (i.e., the spaceship hasn't hit a planet yet), any angle is valid.
+            return true;
+        }
+
+        // Calculate the forward direction of the spaceship for the desired angle
+        float desiredAngleInRadians = (desiredAngle + 90) * Mathf.Deg2Rad;
+        Vector2 desiredDirection = new Vector2(Mathf.Cos(desiredAngleInRadians), Mathf.Sin(desiredAngleInRadians));
+
+        // The dot product will be negative if the two vectors are more than 90 degrees apart.
+        // This means the desired direction is into the planet (i.e., not a valid launch angle).
+        return Vector2.Dot(desiredDirection, collisionNormal.Value) >= 0;
     }
 }
